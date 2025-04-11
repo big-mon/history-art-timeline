e97578044d5f4e9262f75ea91bae6c067207ab42
MON <mrmon93+github@gmail.com>
2025-04-12 01:43:25 +0900

    prettier

--- src/lib/data/sampleData.ts
+++ src/lib/data/sampleData.ts
@@ -4,15 +4,19 @@ export const timelineData: TimelineItem[] = [
   {
     date: '1503',
     title: 'モナ・リザ',
-    description: 'レオナルド・ダ・ヴィンチによって描かれた肖像画。現在はルーブル美術館に所蔵されている。',
+    description:
+      'レオナルド・ダ・ヴィンチによって描かれた肖像画。現在はルーブル美術館に所蔵されている。',
     category: '絵画',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/687px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/687px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
   },
   {
     date: '1889',
     title: '星月夜',
-    description: 'フィンセント・ファン・ゴッホによって描かれた油彩画。現在はニューヨーク近代美術館に所蔵されている。',
+    description:
+      'フィンセント・ファン・ゴッホによって描かれた油彩画。現在はニューヨーク近代美術館に所蔵されている。',
     category: '絵画',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg'
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg'
   }
 ];
